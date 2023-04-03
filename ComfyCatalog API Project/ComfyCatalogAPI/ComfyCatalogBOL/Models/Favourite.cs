30494d80692ebc33d75b30090d01380b4c85fef5
Nuno Veloso <a10411@alunos.ipca.pt>
2023-04-03 09:23:56 +0100

    models (to be finished)

---
+++ ComfyCatalog API Project/ComfyCatalogAPI/ComfyCatalogBOL/Models/Favourite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComfyCatalogBOL.Models
+{
+    public class Favourite
+    {
+        public int FavouriteID  {get; set }
+
+        public int UserID { get; set; } 
+
+        public int ProductID { get; set; }
+
+        public Favourite() { }
+
+        public Favourite(SqlDataReader rdr)
+        {
+            this.FavouriteID = Convert.ToInt32(rdr["favouriteID"]);
+            this.UserID = Convert.ToInt32(rdr["userID"]);
+            this.ProductID = Convert.ToInt32(rdr["productID"]);
+        }
+
+
+    }
+}
---
+++ ComfyCatalog API Project/ComfyCatalogAPI/ComfyCatalogBOL/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ComfyCatalogBOL.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password_Hash { get; set; }
+
+        public string Password_Salt { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(SqlDataReader rdr)
+        {
+            this.UserID = Convert.ToInt32(rdr["UserID"]);
+            this.Username = rdr["username"].ToString() ?? String.Empty;
+            this.Password_Hash = rdr["password_hash"].ToString() ?? String.Empty;
+            this.Password_Salt = rdr["password_salt"].ToString() ?? String.Empty;
+        }
+
+
+    }
+
+
+
+}
